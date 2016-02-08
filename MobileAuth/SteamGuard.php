e17f7242c0e88f8fc664cf3ade45933143165a5c
Joel <waylaidwanderer@live.com>
2016-02-08 12:33:42 -0800

    Simplified code to generate 2FA code

--- MobileAuth/SteamGuard.php
+++ MobileAuth/SteamGuard.php
@@ -11,6 +11,8 @@ namespace waylaidwanderer\SteamCommunity\MobileAuth;
 
 class SteamGuard
 {
+    private static $codeTranslations = [50, 51, 52, 53, 54, 55, 56, 57, 66, 67, 68, 70, 71, 72, 74, 75, 77, 78, 80, 81, 82, 84, 86, 87, 88, 89];
+    private static $codeTranslationsLength = 26;
     private $sharedSecret = '';
 
     public function __construct($sharedSecret = '')
@@ -30,19 +32,7 @@ class SteamGuard
         }
 
         $sharedSecret = base64_decode($this->sharedSecret);
-
-        $time /= 30;
-        $timeArray = [];
-        for ($i = 8; $i > 0; $i--) {
-            $timeArray[$i - 1] = $this->intToByte($time);
-            $time >>= 8;
-        }
-        $timeArray = array_reverse($timeArray);
-        $timeHash = '';
-        foreach ($timeArray as $value) {
-            $timeHash .= chr($value);
-        }
-
+        $timeHash = pack('N*', 0) . pack('N*', floor($time / 30));
         $hmac = unpack('C*', pack('H*', hash_hmac('sha1', $timeHash, $sharedSecret, false)));
         $hashedData = [];
         $modeIndex = 0;
@@ -54,13 +44,10 @@ class SteamGuard
         $b = $this->intToByte(($hashedData[19] & 0xF));
         $codePoint = ($hashedData[$b] & 0x7F) << 24 | ($hashedData[$b+1] & 0xFF) << 16 | ($hashedData[$b+2] & 0xFF) << 8 | ($hashedData[$b+3] & 0xFF);
 
-        $steamGuardCodeCharset = "23456789BCDFGHJKMNPQRTVWXY";
-        $steamGuardCodeCharsetLength = strlen($steamGuardCodeCharset);
-        $code = "";
-        for ($i = 0; $i < 5; $i++)
-        {
-            $code = $code."".$steamGuardCodeCharset{floor($codePoint) % $steamGuardCodeCharsetLength};
-            $codePoint /= $steamGuardCodeCharsetLength;
+        $code = '';
+        for ($i = 0; $i < 5; ++$i) {
+            $code .= chr(self::$codeTranslations[$codePoint % self::$codeTranslationsLength]);
+            $codePoint /= self::$codeTranslationsLength;
         }
         return $code;
     }
